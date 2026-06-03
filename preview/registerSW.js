535ec05e31b6c440676458ac6fbbab7f4ae85437
acer1456 <acer1456@users.noreply.github.com>
2026-06-03 02:47:04 +0000

    deploy: 0eb76513f1cd30b332a100a81685130b74e567f0

---
+++ preview/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/image-shot-video-generator/preview/sw.js', { scope: '/image-shot-video-generator/preview/' })})}
